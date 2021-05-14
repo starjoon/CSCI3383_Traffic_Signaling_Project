afa21aecbd5acf37d0de5b6478d2dee62f17d3cb
Joon Choi <starjoon2013@gmail.com>
2021-05-14 19:01:27 -0400

    Floyd-Warshall on a samller sample

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
 import random
-import matplotlib as plt
+import pandas as pd
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 # stores information of each street
@@ -49,7 +52,7 @@ duration, numNodes, numStreets, numCars, bonus = [
 # We can see how many nodes and edges there are in the Directed Weighted Graph, along with the number of cars traveling
 print("Number of nodes: ", numNodes)
 print("Number of edges: ", numStreets)
-print("Number of nodes: ", numCars)
+print("Number of cars: ", numCars)
 
 # Line by line, we can consolidate every street (two nodes connected by an edge) into a dictionary with object-oriented programming
 streets = {}
@@ -84,7 +87,7 @@ for street, attributes in streets.items():
     start_node = attributes.get('start')
     end_node = attributes.get('end')
     distance = attributes.get('length')
-    tablee[start_node][end_node] = distance
+    table[start_node][end_node] = distance
 
 # Floyd-Warshall Algorithm O(|V|^3)
 for i in range(numNodes):
@@ -95,4 +98,55 @@ for i in range(numNodes):
 
 
 # Given that we have 8000 nodes with 63968 edges in the Directed Weighted Graph, our computers do not possess enough processing power to run the algorithm
-# In order to compromise, we can run the algorithm with a smaller sample size (reduce the number of streets) OR construct a graph around the given car paths
+# In order to compromise, we can run the algorithm with a smaller sample size by constructing a graph around the given car paths
+
+# Random sample 20 cars to start with a smaller sample size of nodes/edges for our new graph
+# Since we will be using a different set of nodes/edges, we have to re-index our data before setting up the table
+
+sampleNodes = set()
+sampleEdges = set()
+
+sampleCars = random.sample(cars, 20)
+for car in sampleCars:
+    for path in car.get('path'):
+        start_end = path.split('-')
+        if start_end[0] not in sampleNodes:
+            sampleNodes.add(start_end[0])
+        if start_end[1] not in sampleNodes:
+            sampleNodes.add(start_end[1])
+        if path not in sampleEdges:
+            sampleEdges.add(path)
+
+sampleNodes = sorted(list(sampleNodes))
+
+indexMap = {}
+mapCounter = 0
+for node in sampleNodes:
+    indexMap[node] = mapCounter
+    mapCounter += 1
+
+print(f"Number of nodes in reduced graph (20 cars): {len(sampleNodes)}")
+print(f"Number of edges in reduced graph (20 cars): {len(sampleEdges)}")
+
+
+# Now we can build a smaller table and run the Floy-Warshall Algorithm
+sampleTable = [[math.inf for i in range(len(sampleNodes))]
+               for j in range(len(sampleNodes))]
+
+# Zero value across the diagonal; zero distance to itself
+for node in range(len(sampleNodes)):
+    sampleTable[node][node] = 0
+
+# Assign distance values between pairs of nodes from the given input data
+for i in range(len(sampleCars)):
+    for j in range(len(sampleCars[i].get('path'))):
+        car_ij = sampleCars[i].get('path')[j].split('-')
+        sampleTable[indexMap[car_ij[0]]][indexMap[car_ij[1]]
+                                         ] = streets[sampleCars[i].get('path')[j]].get('length')
+
+# Floyd-Warshall Algorithm O(|V|^3)
+for i in range(len(sampleNodes)):
+    for j in range(len(sampleNodes)):
+        for k in range(len(sampleNodes)):
+            if sampleTable[j][k] > sampleTable[j][i]+sampleTable[i][k]:
+                sampleTable[j][k] == sampleTable[j][i]+sampleTable[i][k]
